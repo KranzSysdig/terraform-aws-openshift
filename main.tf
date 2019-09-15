a7653c09fa2a4f69bfcee5386ad2b1b3f6b70369
bend72 <ben.dingley@jaminconsulting.co.uk>
2019-09-15 18:00:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Setup our providers so that we have deterministic dependecy resolution. 
+# Setup our Terraform providers so that we have deterministic dependecy resolution. 
 provider "aws" {
   region  = "${var.region}"
   version = "~> 2.19"
@@ -12,17 +12,22 @@ provider "template" {
   version = "~> 2.1"
 }
 
-//  Create the OpenShift cluster using our module.
+#  Create the OpenShift cluster using defined child modules nested under the openshift module at "modules/openshift".
 module "openshift" {
-  source          = "./modules/openshift"
-  region          = "${var.region}"
-  amisize         = "m4.large"    //  Smallest that meets the min specs for OS
-  vpc_cidr        = "10.0.0.0/16"
-  subnet_cidr     = "10.0.1.0/24"
-  key_name        = "openshift"
+  source = "./modules/openshift"
+  region = "${var.region}"
+  ec2_type_bastion = "${var.ec2_bastion}"  #m4.xlarge is the smallest ami that meets the min specs for openshift of 4 vCPU and 16GB memory
+  ec2_type_master = "${var.ec2_master}"
+  ec2_type_infra = "${var.ec2_infra}"
+  ec2_type_node = "${var.ec2_node}"
+  node_amisize = "m4.large"    # Large enough node for infra and compute to run containers on
+  vpc_cidr = "172.16.0.0/16"
+  public_subnet_cidr = "172.16.0.0/24"
+  private_subnet_cidr = "172.16.16.0/20"
+  key_name = "${var.clusterid}.${var.dns_domain}"
   public_key_path = "${var.public_key_path}"
-  cluster_name    = "openshift-cluster"
-  cluster_id      = "openshift-cluster-${var.region}"
+  cluster_name = "${var.clusterid}"
+  cluster_id = "openshift-cluster-${var.region}"
 }
 
 //  Output some useful variables for quick SSH access etc.
