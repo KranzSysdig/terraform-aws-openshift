9b751366bbaff3ea4e191df1e115fb50c714a1d7
bend72 <ben.dingley@jaminconsulting.co.uk>
2019-09-15 20:21:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,33 @@
-//  The region we will deploy our cluster into.
+#  The region that will be used for to deploy the OKD cluster into.
 variable "region" {
-  description = "Region to deploy the cluster into"
   default = "eu-west-2"
 }
 
-//  The public key to use for SSH access.
+#  The public key to use for SSH access.
 variable "public_key_path" {
   //default = "~/.ssh/id_rsa.pub"
   default = "key.pem.pub"
+}
+
+# The ec2 instnace types to use in the deployment
+variable "ec2_instances" {
+  type = list(object({
+    ec2_bastion = string
+    ec2_master = string
+    ec2_infra = string
+    ec2_node = string
+  }))
+  default = [
+    {
+      ec2_bastion = "t2.micro"
+      ec2_master = "m4.xlarge"
+      ec2_infra = "m4.large"
+      ec2_node = "m4.large"
+    }
+  ]
+}
+
+# Set the domain name to use with Route53 Hosted Zones.
+variable "domain_dns" {
+  default = "tarben.co.uk"
 }
