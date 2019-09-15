6e309e0961f0a41a367cfc58126f018722b7075d
bend72 <ben.dingley@jaminconsulting.co.uk>
2019-09-15 20:21:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,15 +12,14 @@ provider "template" {
   version = "~> 2.1"
 }
 
-#  Create the OpenShift cluster using defined child modules nested under the openshift module at "modules/openshift".
+#  Create the OpenShift cluster using defined child modules nested at "modules/openshift".
 module "openshift" {
   source = "./modules/openshift"
   region = "${var.region}"
-  ec2_type_bastion = "${var.ec2_bastion}"  #m4.xlarge is the smallest ami that meets the min specs for openshift of 4 vCPU and 16GB memory
-  ec2_type_master = "${var.ec2_master}"
-  ec2_type_infra = "${var.ec2_infra}"
-  ec2_type_node = "${var.ec2_node}"
-  node_amisize = "m4.large"    # Large enough node for infra and compute to run containers on
+  ec2_type_bastion = "${var.ec2_instances.ec2_bastion}"
+  ec2_type_master = "${var.ec2_instances.ec2_master}"
+  ec2_type_infra = "${var.ec2_instances.ec2_infra}"
+  ec2_type_node = "${var.ec2_instances.ec2_node}"
   vpc_cidr = "172.16.0.0/16"
   public_subnet_cidr = "172.16.0.0/24"
   private_subnet_cidr = "172.16.16.0/20"
@@ -28,9 +27,10 @@ module "openshift" {
   public_key_path = "${var.public_key_path}"
   cluster_name = "${var.clusterid}"
   cluster_id = "openshift-cluster-${var.region}"
+  domain_dns = "${var.domain_dns}"
 }
 
-//  Output some useful variables for quick SSH access etc.
+#  Output some useful variables for quick SSH access etc.
 output "master-url" {
   value = "https://${module.openshift.master-public_ip}.xip.io:8443"
 }
