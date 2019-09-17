0c006c115155a75a3dfd0ae143e6243c68ace49b
bend72 <ben.dingley@jaminconsulting.co.uk>
2019-09-17 16:31:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -60,25 +60,44 @@ variable "cidr_private" {
     ]
   }
 
-# The ec2 instnace types to use in the deployment
+# Set the ec2 instnace types to use in the deployment
 variable "ec2_instances" {
   type = list(object({
     ec2_bastion = string
     ec2_master = string
     ec2_infra = string
     ec2_node = string
-  }))
+    }))
   default = [
     {
       ec2_bastion = "t2.micro"
       ec2_master = "m4.xlarge"
       ec2_infra = "m4.large"
       ec2_node = "m4.large"
-    }
-  ]
-}
+      }
+    ]
+  }
 
+# Set the AWS AMI details to use with RHEL release
+variable "rhel_release" {
+  default = "rhel-7.5"
+  }
 
+# Set the AWS AMI details to use with CentOS release
+variable "centos_release" {
+  type = list(object ({
+    centos_version = string
+    product_code = string
+    product_owner = sting
+    }))
+  default = [
+    {
+      centos_version = "7"
+      product_code = "aw0evgkw8e5c1q413zgy5pjce"
+      product_owner = "aws-marketplace"
+      }
+    ]
+  }
 
 #  The public key to use for SSH access.
 variable "public_key_path" {
